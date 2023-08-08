051de756c8a6073b9f60832873d2bcf7f9370c1e
06279C744 <06279C744@host.docker.internal>
2023-08-08 17:03:50 +0530

    Disabling Spring Security

--- src/main/java/com/madhavasoft/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
+++ src/main/java/com/madhavasoft/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -1,13 +1,13 @@
 package com.madhavasoft.rest.webservices.restfulwebservices.security;
-import static org.springframework.security.config.Customizer.*;
+/*import static org.springframework.security.config.Customizer.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;*/
 
-@Configuration
+//@Configuration
 	public class SpringSecurityConfiguration {
-		
+	/*	
 		@Bean
 		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 			
@@ -25,5 +25,5 @@ import org.springframework.security.web.SecurityFilterChain;
 			
 			return http.build();
 		}
-
+*/
 }
